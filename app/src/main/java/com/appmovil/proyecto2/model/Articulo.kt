b806b90dbd4b4f52cb4f6be3330a53ec2bf7e631
diana cadena <diana.marcela.cadena@correounivalle.edu.co>
2023-12-07 16:11:39 -0500

    Inicio addFragment

---
+++ app/src/main/java/com/appmovil/proyecto2/model/Articulo.kt
@@ -0,0 +1,8 @@
+package com.appmovil.proyecto2.model
+
+data class Articulo(
+    val codigo:Int,
+    val nombre: String,
+    val precio:Int,
+    val cantidad:Int
+)
---
+++ app/src/main/java/com/appmovil/proyecto2/view/fragment/AddFragment.kt
@@ -0,0 +1,30 @@
+package com.appmovil.proyecto2.view.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.appmovil.proyecto2.R
+import com.appmovil.proyecto2.databinding.FragmentAddBinding
+import com.appmovil.proyecto2.databinding.FragmentHomeBinding
+
+
+class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
