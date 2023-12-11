e48286b49fa79fe4ba0fc37ecb5c8048561b6ca8
diana cadena <diana.marcela.cadena@correounivalle.edu.co>
2023-12-11 13:44:03 -0500

    test metodo listarProductos

--- app/src/test/java/com/appmovil/proyecto2/viewmodel/InventoryViewModelTest.kt
+++ app/src/test/java/com/appmovil/proyecto2/viewmodel/InventoryViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.appmovil.proyecto2.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.appmovil.proyecto2.repository.InventoryRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -8,6 +10,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
@@ -53,7 +56,24 @@ class InventoryViewModelTest {
 
     @Test
     fun `test metodo listarProductos`() {
-        // Implementa pruebas para listarProductos si es necesario
+        // Dado que listarProductos no es suspendido, no es necesario usar coroutines en las pruebas
+
+        // Simula la respuesta del repositorio
+        val productosMock = "Código: 1 Nombre: Producto1 Precio: 10 Cantidad: 5\n\n" +
+                "Código: 2 Nombre: Producto2 Precio: 15 Cantidad: 8\n\n"
+        `when`(repository.listarProductos()).thenReturn(MutableLiveData(productosMock))
+
+        // Observador para el LiveData que se espera sea actualizado
+        val observer = mock(Observer::class.java) as Observer<String>
+
+        // Observar el LiveData y verificar si se actualiza correctamente
+        inventoryViewModel.listarProductos().observeForever(observer)
+
+        // Verifica que el método en el repositorio haya sido llamado
+        verify(repository).listarProductos()
+
+        // Verifica que el LiveData en el ViewModel ha sido actualizado con la data simulada
+        verify(observer).onChanged(productosMock)
     }
 }
 
