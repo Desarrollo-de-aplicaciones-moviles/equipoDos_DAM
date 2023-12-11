083bb581ea759c9b895d2bff293a83f0da11e8c2
Juan2040751 <viafara.juan@correounivalle.edu.co>
2023-12-10 21:57:06 -0500

    WI-1 widget interaction

---
+++ app/src/main/java/com/appmovil/proyecto2/view/InventoryWidget.kt
@@ -0,0 +1,39 @@
+package com.appmovil.proyecto2.view
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.appmovil.proyecto2.R
+
+class InventoryWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                Intent(context, LoginActivity::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.inventory_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.gestionarIcon, pendingIntent)
+            }
+
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
